e20572d1c76047c8dde1db2c485f2af675242de4
Fabiofcn <fabiofcn@gmail.com>
2020-06-07 11:57:13 -0300

    Resouse1

--- src/main/java/com/example/cruso1/resources/CategoriaResource.java
+++ src/main/java/com/example/cruso1/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.example.cruso1.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categoriasss")
@@ -10,8 +15,15 @@ public class CategoriaResource {
 	//Procurar cruso de REST
 	//Para reconhecer um metodo fora do CRUD
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest -  esta funcionado!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
